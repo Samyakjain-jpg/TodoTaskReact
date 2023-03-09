8f71cf85751e517243e2dcffec5394f9b1b5528c
Samyak jain <samyakj163@gmail.com>
2023-03-10 01:51:48 +0530

    third commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css'
 
 function Header() {
   return (
-    <div><h1>Get ready to complete daily Goals</h1>
+    <div><h1>Get ready to complete daily Goalss</h1>
     </div>
   )
 }
